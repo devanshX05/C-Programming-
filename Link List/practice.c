a0f8b0c926ed777f29bc81da3a0eb5b984e4bff5
Devansh Dubey <devanshdubey81136@gmail.com>
2026-05-01 16:46:31 +0530

    2

--- Link List/practice.c
+++ Link List/practice.c
@@ -1,28 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node{
+struct node {
     int value;
     struct node *ptr;
-    struct node *pre;
-}*head=NULL;
-void insert_end(int item){
+};
+void end(struct node **head,int item){
     struct node *new,*temp;
-    new=(struct node *)malloc(sizeof(struct node));
+    new=(struct node*)malloc(sizeof(struct node));
     if(new==NULL){
-        printf("No memory");
+        printf("no memory");
     }
     new->value=item;
     new->ptr=NULL;
-    new->pre=NULL;
-
-    if(head==NULL){
-        head=new;
+    if(*head==NULL){
+        *head=new;
         return;
     }
-    temp=head;
+    temp=*head;
     while(temp->ptr!=NULL){
         temp=temp->ptr;
     }
     temp->ptr=new;
-    new->pre=temp;
+}
+void delete(struct node **head){
+    struct node *temp,*pre;
+    if(*head==NULL){
+        printf("Dketeion  ot possi0");
+        return;
+    }
+    if((*head)->ptr==NULL){
+        free(*head);
+        *head=NULL;
+    }
+    temp=*head;
+    while(temp->ptr!=NULL){
+        pre=temp;
+        temp=temp->ptr;
+    }
+    pre->ptr=NULL;
+    free(temp);
+
 }
