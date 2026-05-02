4af9429995a6f938a2966af5a99cbe5e5f421bff
Devansh Dubey <devanshdubey81136@gmail.com>
2026-05-02 23:09:33 +0530

    1

--- Link List/practice.c
+++ Link List/practice.c
@@ -2,61 +2,25 @@
 #include<stdlib.h>
 struct node{
     int value;
-    struct node *ptr;
-}*head=NULL;    
-void insert_end(struct node **head,int item){
+    struct node *next;
+    struct node *pre;  
+}*head=NULL ;
+void insert_end(int item){
     struct node *new,*temp;
-    new = (struct node*)malloc(sizeof(struct node));
+    new=(struct node*)malloc(sizeof(struct node));
     if(new==NULL){
-        printf("Memory not alloavtyed");
+        printf("no");
     }
     new->value=item;
-    new->ptr=NULL;
-
-    if(*head==NULL){
-        *head=new;
-        return;
+    new->next=NULL;
+    if(head==NULL){
+        head=new;
     }
-    temp=*head;
-    while(temp->ptr!=NULL){
-        temp=temp->ptr;
+    temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
     }
-    temp->ptr=new;
-   
-}
-void delete_end(struct node **head){
-    struct node *temp,*pre;
-    if(*head==NULL){
-        printf("not possible");
-        return;
-    }
-    temp=*head;
-    while(temp->ptr!=NULL){
-        pre=temp;
-        temp=temp->ptr;
-    }
-    pre->ptr=NULL;
-    free(temp);
-}
-void display(struct node *head){
-    while(head!=NULL){
-        printf("%d ",head->value);
-        head=head->ptr;
-    }
-    printf("NULL\n");
-}
-int main(){
-    struct node *head=NULL;
-    int n,value;
-    printf("Number of node:");
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&value);
-        insert_end(&head,value);
-    }
-    display(head);
-    delete_end(&head);
-    printf("After deletion:\n");
-    display(head);
-    return 0;
+    temp->next=new;
+    new->pre=temp;
+    
 }
--- Stack/practice.c
+++ Stack/practice.c
@@ -1,31 +1,38 @@
 #include<stdio.h>
-#define size 10
-    int a[size],top=-1;
-    void push(int item){
-        if(top==size-1){
-            printf("Stack Overflow");
-            return;
-        }
-        top=top+1;
-        a[top]=item;
-        printf("value psuhed:%d",item);
+struct node{
+    int value;
+    struct node *next;
+};
+struct node *top=NULL;
+void push(int item){
+    struct node *new;
+    new=(struct node *)malloc(sizeof(struct node));
+    if(new==NULL){
+        printf("no");
     }
-    void pop(){
-        if(top==-1){
-            printf("Stack UnderFLow");
-            return;
-        }
-        int temp=a[top];
-        printf("Value popped:%d",temp);
-        top=top-1;
+    new->value=item;
+    if(top==NULL){
+        top=new;
+        return;
     }
-    void display(){
-        if(top==NULL){
-            printf("Stack empty");
-            return;
-        }
-        for(int i=top;i>0;i++){
-            printf("%d->",a[i]);
-        }
+    new->next=top;
+    top=new;
+}
+void pop(){
+    struct node *temp;
+    if(top==NULL){
+        printf("Stack underflow");
+        return ;
     }
-
+    temp=top;
+    top=temp->next;
+    free(temp);
+}
+void display(){
+    struct node *temp;
+    temp=top;
+    while(temp->next!=NULL){
+        printf("%d ",temp->value);
+        temp=temp->next;
+    }
+}
